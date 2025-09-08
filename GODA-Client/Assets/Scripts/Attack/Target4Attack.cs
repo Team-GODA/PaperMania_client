2b4af6bb2ef565b591fa0c4b38d770613ca0d2ef
wkdrudgnsdla <s24032@gsm.hs.kr>
2025-09-08 19:45:14 +0900

    기능 변경
    오브젝트 액티브로 주는것->타겟공격의 공격 콜라이더 활성,비활성화로 구현

---
+++ GODA-Client/Assets/Scripts/Attack/Target4Attack.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Target4Attack : MonoBehaviour
+{
+    public float attackDmg;
+    public BoxCollider2D boxCol;
+
+    public void Awake()
+    {
+        InvokeRepeating("GetDmg", 0f, 0.01f);
+        boxCol = GetComponent<BoxCollider2D>();
+
+    }
+
+    public void GetDmg(float dmg)
+    {
+        attackDmg = dmg;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.TryGetComponent<Enemy>(out Enemy enemy))
+        {
+            enemy.nowHP -= attackDmg;
+        }
+        else return;
+    }
+}
---
+++ GODA-Client/Assets/Scripts/Monster/Enemy.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float maxHP;
+    public float nowHP;
+}
