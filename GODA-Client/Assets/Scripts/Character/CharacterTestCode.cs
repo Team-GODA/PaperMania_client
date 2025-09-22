5952993d1d59cca99145f1351dd1e9dbeddc8afa
LeeSiu <167082095+leesiuuuu@users.noreply.github.com>
2025-09-22 15:35:14 +0900

    #3 테스트기 상태 추가
    되돌리기, 평타 추가

--- GODA-Client/Assets/Scripts/Character/CharacterTestCode.cs
+++ GODA-Client/Assets/Scripts/Character/CharacterTestCode.cs
@@ -4,6 +4,18 @@ public class CharacterTestCode : MonoBehaviour
 {
     public Animator animator;
 
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    public void DOHit()
+    {
+        animator.SetTrigger("Hit");
+    }
+
     public void DOSkill1()
     {
         animator.SetTrigger("Skill1");
@@ -11,5 +23,10 @@ public class CharacterTestCode : MonoBehaviour
     public void DOSkill2()
     {
         animator.SetTrigger("Skill2");
+    }
+
+    public void Reset()
+    {
+        transform.position = startPos;
     }
 }
