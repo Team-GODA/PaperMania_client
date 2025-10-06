ae519d7863ab2fb4899fe58e4b4e8473efa502ed
wkdrudgnsdla <s24032@gsm.hs.kr>
2025-10-07 00:21:22 +0900

    캐릭터 1 스킬 2 완성

--- GODA-Client/Assets/Scripts/Attack/AttackManager.cs
+++ GODA-Client/Assets/Scripts/Attack/AttackManager.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class AttackManager : MonoBehaviour
 {
     public AttackTarget attackTarget;
+    public Character1Skills character1Skills;
 
     private void Awake()
     {
         attackTarget = gameObject.GetComponent<AttackTarget>();
+        character1Skills = gameObject.GetComponent<Character1Skills>();
     }
 
+    //타겟공격
     public void OnCilckAttack1()
     {
         attackTarget.TargetAttack(1, 10);
@@ -37,5 +40,13 @@ public class AttackManager : MonoBehaviour
     public void OnCilckAttack6()
     {
         attackTarget.TargetAttack(6, 10);
+    }
+
+
+
+    //플레이어 스킬
+    public void OnClickSkill2()
+    {
+        StartCoroutine(character1Skills.Skill2());
     }
 }
