a90f434e61a0b88221ea48be4a6a9e94f3c7b1cf
wkdrudgnsdla <s24032@gsm.hs.kr>
2025-10-13 17:26:14 +0900

    enemy 코드 수정
    수정내역:열 포지션 추가, 공격력 증,감을 위한 attackMultiplier 추가

--- GODA-Client/Assets/Scripts/Monster/Enemy.cs
+++ GODA-Client/Assets/Scripts/Monster/Enemy.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
 
+
 public class Enemy : MonoBehaviour
 {
     public float maxHP;
     public float nowHP;
+
+    private float baseAttack;
+    private float attackMultiplier;
+    public float attackDmg => baseAttack * attackMultiplier;
+
+    [SerializeField] private Row row = Row.Mid;
+    public Row RowPosition => row;
 
     protected virtual void Start()
     {
