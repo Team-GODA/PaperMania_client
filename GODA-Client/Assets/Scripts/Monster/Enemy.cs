16fa5fd65ea7b809250898c1dbadc5658efc75d1
wkdrudgnsdla <s24032@gsm.hs.kr>
2025-10-14 19:51:44 +0900

    쉴드 값 추가

--- GODA-Client/Assets/Scripts/Monster/Enemy.cs
+++ GODA-Client/Assets/Scripts/Monster/Enemy.cs
@@ -5,10 +5,12 @@ public enum Row { Front, Mid, Back }
 public class Enemy : MonoBehaviour
 {
     public float maxHP;
-    public float nowHP;
+    public float nowHP; //체력
+    public float shield;
+    public float totalHP => nowHP + shield; //체력과 쉴드 양을 합친 총 체력
 
     private float baseAttack;
-    private float attackMultiplier;
+    public float attackMultiplier;
     public float attackDmg => baseAttack * attackMultiplier;
 
     [SerializeField] private Row row = Row.Mid;
