5847ac59f304660d657d68bc1164fb3e91243d8d
wkdrudgnsdla <s24032@gsm.hs.kr>
2025-12-22 10:43:29 +0900

    스테이지 시스템을 위한 Enemy코드 수정
    Start에서 하던 값을 바꿔주는것을 OnEnable로 변경하여 사용하였습니다. 이 외에도 Polling 사용을 위하여 코드를 수정하였습니다

--- GODA-Client/Assets/Scripts/Enemy/Enemy.cs
+++ GODA-Client/Assets/Scripts/Enemy/Enemy.cs
@@ -1,12 +1,16 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public event Action<Enemy> OnDied;
+
     [Header("Status")]
     public float MaxHP;
-    public float NowHP; //체력
+    public float NowHP; // 체력
     public float Shield;
-    public float TotalHP => NowHP + Shield; //체력과 쉴드 양을 합친 총 체력
+
+    public float TotalHP => NowHP + Shield; // 체력과 쉴드 양을 합친 총 체력
 
     public float BaseAttack;
     public float AttackMultiplier;
@@ -16,13 +20,18 @@ public class Enemy : MonoBehaviour
     public float SlowDebuff = 1;
     public float MoveSpeed => Speed * SlowDebuff;
 
-    protected virtual void Start()
+    private bool isAlive = false;
+
+    protected virtual void OnEnable()
     {
+        isAlive = true;
         NowHP = MaxHP;
     }
 
     public void TakeDamage(float damage)
     {
+        if (!isAlive) return;
+
         if (Shield >= damage)
         {
             Shield -= damage;
@@ -32,6 +41,30 @@ public class Enemy : MonoBehaviour
             float leftDmg = damage - Shield;
             Shield = 0;
             NowHP -= leftDmg;
+        }
+
+        if (NowHP <= 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        if (!isAlive) return;
+        isAlive = false;
+
+        OnDied?.Invoke(this);
+
+        gameObject.SetActive(false);
+    }
+
+    protected virtual void OnDisable()
+    {
+        if (isAlive)
+        {
+            isAlive = false;
+            OnDied?.Invoke(this);
         }
     }
 }
