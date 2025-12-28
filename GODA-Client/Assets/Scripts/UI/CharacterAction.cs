43d06365e65022197609243ee9711be82b299a02
LeeSiu <167082095+leesiuuuu@users.noreply.github.com>
2025-12-29 01:10:29 +0900

    feat: 그래픽 설정 추가
    #9

--- GODA-Client/Assets/Scripts/UI/CharacterAction.cs
+++ GODA-Client/Assets/Scripts/UI/CharacterAction.cs
@@ -3,27 +3,21 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class CharacterAction : MonoBehaviour
+public class CharacterAction : MonoBehaviour, IPointerClickHandler
 {
-    public Transform parent;
-    public UnityEvent OnAnimComplete;
-    void OnMouseDown()
-    {
-        Debug.Log("click");
-    }
-    void OnMouseUp()
-    {
-        if (EventSystem.current.IsPointerOverGameObject()) return;
-        parent.DORotateQuaternion(Quaternion.Euler(90f, 0, 0), 1f)
-        .SetEase(Ease.OutBounce)
-        .OnComplete(() =>
-        {
-            OnAnimComplete?.Invoke();
-        });
-    }
+	public UnityEvent OnAnimComplete;
 
-    public void LoadScene()
-    {
-        LoadSceneManager.Instance.MainLoadScene("MainScene");
-    }
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		// UI가 클릭을 먹으면 이 함수 자체가 호출되지 않음
+		transform.DORotateQuaternion(Quaternion.Euler(90f, 0, 0), 1f)
+			.SetEase(Ease.OutBounce)
+			.OnComplete(() => OnAnimComplete?.Invoke());
+	}
+
+	public void LoadScene()
+	{
+		LoadSceneManager.Instance.MainLoadScene("MainScene");
+	}
 }
