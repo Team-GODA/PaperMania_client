c1e1138a6a23bdaeb03aaffc7603e13d508aef13
wkdrudgnsdla <s24032@gsm.hs.kr>
2025-12-30 09:11:37 +0900

    2스킬 시전중 움직임 블락

--- GODA-Client/Assets/Scripts/Player/PlayerAnimTest.cs
+++ GODA-Client/Assets/Scripts/Player/PlayerAnimTest.cs
@@ -192,6 +192,10 @@ public class PlayerAnimTest : MonoBehaviour
     {
         if (skill2Timer > 0f || isUsingSkill) return;
 
+        IsDashing = true;
+        dashCooldownTimer = DashCooldown;
+        float dashSpeed = Skill1Distance / Mathf.Max(0.0001f, DashDuration);
+
         isUsingSkill = true;
         CharacterAnimator.SetTrigger("Skill2");
     }
@@ -269,6 +273,9 @@ public class PlayerAnimTest : MonoBehaviour
 
         skill2Timer = Skill2Cooldown;
         isUsingSkill = false;
+
+        IsDashing = false;
+        dashCooldownTimer = DashCooldown;
 
         yield break;
     }
